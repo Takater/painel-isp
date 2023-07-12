9416dfcdbe1c7f289f94a46f30a178bbe58ff10c
Guilherme <guilherme.moret.ps@gmail.com>
2023-07-12 16:25:26 -0400

    Deploy commit

--- frontend/src/components/CitiesList.tsx
+++ frontend/src/components/CitiesList.tsx
@@ -4,8 +4,11 @@ export default function CitiesList() {
 
     const [lista, setLista] = useState<string[]>([])
 
+    const [loadingList, setLoadingList] = useState(true)
+
     useEffect(() => {
         const loadList = async () => {
+            setLoadingList(true);
             const response = await fetch('/cidades');
             const data = await response.json();
             data.cidades && setLista(data.cidades);
@@ -15,7 +18,8 @@ export default function CitiesList() {
     }, [])
 
     return (
-        <select name="city_choice" id="city_choice">
+        <select name="city_choice" id="city_choice" disabled={loadingList}>
+            {loadingList && <option>Carregando cidades...</option>}
             {lista && lista.map((cidade, index) => {
                 return index == lista.length - 1 ?
                 (
@@ -23,7 +27,7 @@ export default function CitiesList() {
                     <option key={cidade} value={cidade}>
                         {cidade}
                     </option>
-                    <option key="all" value="all">Todos</option>
+                    <option key="all" value="all" onLoad={() => setLoadingList(false)}>Todos</option>
                     </>
                 ):
                     <option key={cidade} value={cidade}>
